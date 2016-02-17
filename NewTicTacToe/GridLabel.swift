3924c14f0f027ec2dda3fe415e3cc977c4bec066
dforsyth <student@dforsyth.local>
2016-02-17 12:18:05 -0600

    Finished

---
+++ NewTicTacToe/GridLabel.swift
@@ -0,0 +1,14 @@
+//
+//  GridLabel.swift
+//  NewTicTacToe
+//
+//  Created by dforsyth on 2/16/16.
+//  Copyright © 2016 dforsyth. All rights reserved.
+//
+
+import UIKit
+
+class GridLabel: UILabel {
+
+var canTap = true
+}
